a672ee3c60e327aa08e13486a7b4a9f664948236
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-05 11:45:40 -0700

    video 4 done

--- jsAndTheDom/2-getting-a-handle-on-the-DOM/vid-4-select-element-by-class/start/app.js
+++ jsAndTheDom/2-getting-a-handle-on-the-DOM/vid-4-select-element-by-class/start/app.js
@@ -1,5 +1,13 @@
+'use strict';
+
 const myList = document.getElementsByTagName('li');
 
 for (let i = 0; i < myList.length; i += 1) {
   myList[i].style.color = 'purple';
+}
+
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+  errorNotPurple[i].style.color = 'red';
 }
