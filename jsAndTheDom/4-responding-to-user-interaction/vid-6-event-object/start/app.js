5d4b67872ad4fd506835e1312849ba37afcfbb90
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-12 15:39:00 -0700

    finished video series 4

--- jsAndTheDom/4-responding-to-user-interaction/vid-6-event-object/start/app.js
+++ jsAndTheDom/4-responding-to-user-interaction/vid-6-event-object/start/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
@@ -7,21 +9,30 @@ const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
 
-listDiv.addEventListener('mouseover', () => {
-  listItems[i].textContent = listItems[i].textContent.toUpperCase();
+listDiv.addEventListener('mouseover', (event) => {
+  console.log(event.target.tagName);
+  if(event.target.tagName === 'LI'){
+    event.target.textContent = event.target.textContent.toUpperCase();
+  }
 });
-listDiv.addEventListener('mouseout', () => {
-  listItems[i].textContent = listItems[i].textContent.toLowerCase();
+listDiv.addEventListener('mouseout', (event) => {
+  if(event.target.tagName === 'LI'){
+    event.target.textContent = event.target.textContent.toLowerCase();
+  }
 });
+
+// document.addEventListener('click', (event)=>{
+//   console.log(event.target);
+// });
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
@@ -36,13 +47,9 @@ addItemButton.addEventListener('click', () => {
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
+
 removeItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.querySelector('li:last-child');
   ul.removeChild(li);
 });
-  
-  
-  
-  
