ae3ad20615fca9752de6f2485d7d320b67e0019d
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-12 10:58:04 -0700

    finished video 4

--- jsAndTheDom/4-responding-to-user-interaction/vid-4-listening-for-events/start/app.js
+++ jsAndTheDom/4-responding-to-user-interaction/vid-4-listening-for-events/start/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
@@ -6,15 +8,26 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
+const listItems = document.getElementsByTagName('li');
+
+for(let i = 0; i < listItems.length; i++){
+  listItems[i].addEventListener('mouseover', () =>{
+    listItems[i].textContent = listItems[i].textContent.toUpperCase();
+  });
+
+  listItems[i].addEventListener('mouseout', () =>{
+    listItems[i].textContent = listItems[i].textContent.toLowerCase();
+  });
+}
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
@@ -29,13 +42,9 @@ addItemButton.addEventListener('click', () => {
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
+
 removeItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.querySelector('li:last-child');
   ul.removeChild(li);
 });
-  
-  
-  
-  
