cce15e6bb7125cf46198ebd986cbc6c95d166078
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-12 16:43:25 -0700

    video 5 done

--- jsAndTheDom/5-traversing-the-DOM/vid-5-getting-all-children/start/app.js
+++ jsAndTheDom/5-traversing-the-DOM/vid-5-getting-all-children/start/app.js
@@ -1,3 +1,4 @@
+'use strict';
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
@@ -6,6 +7,28 @@ const descriptionButton = document.querySelector('button.description');
 const listUl = listDiv.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const lis = listUl.children;
+
+function attachListItemButtons(li){
+  let up = document.createElement('button');
+  up.className = 'up';
+  up.textContent = 'Up';
+  li.appendChild(up);
+
+  let down = document.createElement('button');
+  down.className = 'down';
+  down.textContent = 'Down';
+  li.appendChild(down);
+
+  let remove = document.createElement('button');
+  remove.className = 'remove';
+  remove.textContent = 'Remove';
+  li.appendChild(remove);
+}
+
+for(let i = 0; i < lis.length; i++){
+  attachListItemButtons(lis[i]);
+}
 
 listUl.addEventListener('click', (event) => {
   if (event.target.tagName == 'BUTTON') {
@@ -21,7 +44,7 @@ listUl.addEventListener('click', (event) => {
       if (prevLi) {
         ul.insertBefore(li, prevLi);
       }
-    }  
+    }
     if (event.target.className == 'down') {
       let li = event.target.parentNode;
       let nextLi = li.nextElementSibling;
@@ -29,7 +52,7 @@ listUl.addEventListener('click', (event) => {
       if (nextLi) {
         ul.insertBefore(nextLi, li);
       }
-    } 
+    }
   }
 });
 
@@ -38,9 +61,9 @@ toggleList.addEventListener('click', () => {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
@@ -52,10 +75,7 @@ addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
+  attachListItemButtons(li);
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
-  
-  
-  
