ffe9270b03cac760351bb9f69b4e7d86f2e3af3e
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-11 18:25:55 -0700

    finished video 6

--- jsAndTheDom/3-making-changes-to-the-DOM/vid-6-removing-nodes/start/app.js
+++ jsAndTheDom/3-making-changes-to-the-DOM/vid-6-removing-nodes/start/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
@@ -5,15 +7,16 @@ const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
@@ -28,12 +31,9 @@ addItemButton.addEventListener('click', () => {
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
+});
