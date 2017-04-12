a38ddacb74a485a83a8feb9057dbfc0a9f965712
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-11 17:20:11 -0700

    video 3 done

--- jsAndTheDom/3-making-changes-to-the-DOM/vid-3-styling-elements/start/app.js
+++ jsAndTheDom/3-making-changes-to-the-DOM/vid-3-styling-elements/start/app.js
@@ -1,8 +1,20 @@
-const input = document.querySelector('input');
+'use strict';
+const toggleList = document.getElementById('toggleList');
+const listSection = document.querySelector('.list');
+const input = document.querySelector('input.description');
 const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const button = document.querySelector('button.description');
+
+toggleList.addEventListener('click', ()=>{
+  if(listSection.style.display === 'none'){
+    toggleList.textContent = 'Hide List';
+    listSection.style.display = 'block';
+  } else{
+    toggleList.textContent = 'Show List';
+    listSection.style.display = 'none';
+  }
+});
 
 button.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
 });
-
