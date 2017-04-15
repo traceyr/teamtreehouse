7e8cda5cd7e140fa253509aea7f9186b4748af36
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-15 14:50:26 -0700

    practicing events video 2

--- introTojQuery/jQuery-follow-along/app.js
+++ introTojQuery/jQuery-follow-along/app.js
@@ -1,10 +1,4 @@
 'use strict';
-
-console.log($('a').attr('href'));
-
-$('a').not('[href*="my-domain.com"]').attr('target', '_blank');
-
-$('button').on('click', function(){
-  $('a').attr('href', 'https://github.com/traceyr');
-  console.log($('a').attr('href'));
+$('body').on('click', function(){
+  console.log('Clicked');
 });
