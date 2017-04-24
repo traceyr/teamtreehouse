e5eeb1df803863a5d0bc474b6ca332d4190f76c0
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-24 12:00:05 -0700

    video 6/7 finished

--- AJAX_Basics_Project_Files/stage2/video6/js/widget.js
+++ AJAX_Basics_Project_Files/stage2/video6/js/widget.js
@@ -1,9 +1,11 @@
-var xhr = new XMLHttpRequest();
+'use strict';
+
+let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if(xhr.readyState === 4 && xhr.status === 200) {
-    var employees = JSON.parse(xhr.responseText);
-    var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1) {
+    let employees = JSON.parse(xhr.responseText);
+    let statusHTML = '<ul class="bulleted">';
+    for (let i=0; i<employees.length; i += 1) {
       if (employees[i].inoffice === true) {
         statusHTML += '<li class="in">';
       } else {
@@ -18,3 +20,24 @@ xhr.onreadystatechange = function () {
 };
 xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+let roomReq = new XMLHttpRequest();
+roomReq.onreadystatechange = function () {
+  if(roomReq.readyState === 4 && roomReq.status === 200) {
+    let rooms = JSON.parse(roomReq.responseText);
+    let statusHTML = '<ul class="rooms">';
+    for (let i=0; i<rooms.length; i ++) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+roomReq.open('GET', '../data/rooms.json');
+roomReq.send();
