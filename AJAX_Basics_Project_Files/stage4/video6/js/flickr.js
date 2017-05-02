c9ea7b714f595b967334a9d433956623ac172b04
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-05-02 13:31:18 -0700

    working!! issue was .on(click) call. Used .click and fixed error

--- AJAX_Basics_Project_Files/stage4/video6/js/flickr.js
+++ AJAX_Basics_Project_Files/stage4/video6/js/flickr.js
@@ -1,7 +1,6 @@
 'use strict';
 $(document).ready(function () {
-
-  $('button').on('click', function () {
+  $('button').click(function () {
     $('button').removeClass('selected');
     $(this).addClass('selected');
     let flickerAPI = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
@@ -11,14 +10,14 @@ $(document).ready(function () {
       format: 'json'
     };
     let displayPhotos = (data) =>{
-      let photoHTML = '<ul>';
+      let photoHTML = `<ul>`;
       $.each(data.items, function(i, photo){
-        photoHTML += '<li class="grid-25 tablet-grid-50>"';
-        photoHTML += '<a href="' + photo.link+ '" class="images" title="' + photo.author + '"';
-        photoHTML += '<img src="' + photo.media.m+ '" /></a></li>';
+        photoHTML += `<li class="grid-25 tablet-grid-50>"`;
+        photoHTML += `<a href="${photo.link}" class="images" title="${photo.author}">`;
+        photoHTML += `<img src="${photo.media.m}" /></a></li>`;
       });
-      photoHTML += '</ul>';
-      $('photos').html(photoHTML);
+      photoHTML += `</ul>`;
+      $('#photos').html(photoHTML);
     };
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
 
