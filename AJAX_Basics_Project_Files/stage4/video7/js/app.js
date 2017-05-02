7cdbc26e821b0d5e2b62f13b00d1003d16ce4389
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-05-02 13:33:36 -0700

    replaced their shitty code with my better but not great code

--- AJAX_Basics_Project_Files/stage4/video7/js/app.js
+++ AJAX_Basics_Project_Files/stage4/video7/js/app.js
@@ -1,31 +1,25 @@
-$(document).ready(function() {
-
-
- $('button').click(function () {
-    // highlight the button
-    // not AJAX, just cool looking
-    $("button").removeClass("selected");
-    $(this).addClass("selected");
-
-    // the AJAX part
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var animal = $(this).text();
-    var flickrOptions = {
+'use strict';
+$(document).ready(function () {
+  $('button').click(function () {
+    $('button').removeClass('selected');
+    $(this).addClass('selected');
+    let flickerAPI = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+    let animal = $(this).text();
+    let flickrOptions = {
       tags: animal,
-      format: "json"
+      format: 'json'
     };
-    function displayPhotos(data) {
-      var photoHTML = '<ul>';
-      $.each(data.items,function(i,photo) {
-        photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-      }); // end each
-      photoHTML += '</ul>';
+    let displayPhotos = (data) =>{
+      let photoHTML = `<ul>`;
+      $.each(data.items, function(i, photo){
+        photoHTML += `<li class="grid-25 tablet-grid-50>"`;
+        photoHTML += `<a href="${photo.link}" class="images" title="${photo.author}">`;
+        photoHTML += `<img src="${photo.media.m}" /></a></li>`;
+      });
+      photoHTML += `</ul>`;
       $('#photos').html(photoHTML);
-    }
+    };
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
 
-  }); // end click
-
-}); // end ready
+  });
+}); 
