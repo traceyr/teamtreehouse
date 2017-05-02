aae3f48fdfb711d799931c6d09ff8c9df72ef56a
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-05-02 12:51:02 -0700

    video5 done

---
+++ AJAX_Basics_Project_Files/stage4/video5/js/alternative.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$.getJSON('https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?',
+  {
+    tags: animal,
+    format: 'json'
+  },
+  function (data){
+
+  }
+);
