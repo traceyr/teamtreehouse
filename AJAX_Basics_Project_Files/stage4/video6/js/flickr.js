296d46205acc54ecce6bedbae058506a275af1d9
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-05-02 13:17:11 -0700

    getting unexpected < error due to json call. But why?

--- AJAX_Basics_Project_Files/stage4/video6/js/flickr.js
+++ AJAX_Basics_Project_Files/stage4/video6/js/flickr.js
@@ -1,6 +1,7 @@
 'use strict';
 $(document).ready(function () {
-  $('button').on('click',function () {
+
+  $('button').on('click', function () {
     $('button').removeClass('selected');
     $(this).addClass('selected');
     let flickerAPI = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
@@ -10,7 +11,14 @@ $(document).ready(function () {
       format: 'json'
     };
     let displayPhotos = (data) =>{
-
+      let photoHTML = '<ul>';
+      $.each(data.items, function(i, photo){
+        photoHTML += '<li class="grid-25 tablet-grid-50>"';
+        photoHTML += '<a href="' + photo.link+ '" class="images" title="' + photo.author + '"';
+        photoHTML += '<img src="' + photo.media.m+ '" /></a></li>';
+      });
+      photoHTML += '</ul>';
+      $('photos').html(photoHTML);
     };
     $.getJSON(flickerAPI, flickrOptions, displayPhotos);
 
